e67b3a8017264491db8aca762513fd26cf1e4b7f
Prateek Chaturvedi <prateekchatur2001@gmail.com>
2022-08-02 16:55:20 +0530

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld{
+
+    public static void main(String [] s){
+
+        System.out.println("Hello All");
+
+        for(int i=0;i<20;i++)
+        {
+            System.out.println("Value of I:" + i);
+        }
+    }
+}
